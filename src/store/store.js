b16a04cdfe1584966bd305948e6f224635b4cc92
Rohanisamazing <108144246+Rohanisamazing@users.noreply.github.com>
2025-02-23 17:38:04 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
-
+ 
+// Created the Redux store here.
 const store = configureStore({
   reducer: {
     cart: cartReducer,
